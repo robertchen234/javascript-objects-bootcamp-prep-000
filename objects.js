d3c93dfb3ab8002c8bb60f7305c12a9235b3f67b
Robert Chen <robertchen234@gmail.com>
2018-08-19 01:25:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,11 +2,11 @@ var playlist = {
   'Maroon5': 'Closure',
   'Ed Sheeran': 'Perfect'
 }
- function updatePlaylist(Liked, Maroon5, Closure) {
+ function updatePlaylist(Liked, 'Phil Ochs', 'My Bloody Valentine') {
   Liked[BrunoMars] = Grenade
   return Liked
 }
- function removeFromPlaylist(Liked, Maroon5) {
+ function removeFromPlaylist(Liked, Phil Ochs) {
   delete Liked[Maroon5]
   return Liked
 } 
